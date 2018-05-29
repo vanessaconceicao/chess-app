7507dc2a36a13c94e07e309031903541bbadbc67
Vanessa da Conceição <vanessa.sc22@gmail.com>
2018-05-29 12:14:50 -0300

    highlight next positions

--- src/Board.js
+++ src/Board.js
@@ -19,28 +19,47 @@ class Board extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        initialPosition: ''
+        initialPosition: '',
+        nextPositions: []
     }
   }
   setInitialPosition (selectedCell) {
     this.setState({ initialPosition: selectedCell })
   }
 
+  getNextPositions () {
+    const { initialPosition, nextPositions } = this.state
+    //implement fetch
+    //const nextPositions = fetch()
+    this.setState({ nextPositions: ['A1', 'B5', 'A8', 'C4', 'D8', 'G2'] })
+  }
+
+  shouldHighlight(cell) {
+    const { nextPositions } = this.state
+    return nextPositions.indexOf(cell) > -1
+  }
+
   render () {
     const { initialPosition } = this.state
+    
     return (
       <div className="App">
           {columns.map(column =>
             <div className="Board-column" key={column}>
-              {column.map(cell =>
-                <div className="Board-cell" key={cell} onClick={() => this.setInitialPosition(cell)}>
-                  <Cell id={cell} selected={initialPosition === cell}>
-                    <Knight />
-                  </Cell>
-                </div>
-              )}
+              {column.map(cell => {
+                return (
+                  <div className="Board-cell" key={cell} onClick={() => this.setInitialPosition(cell)}>
+                    <Cell id={cell} selected={initialPosition === cell} highlight={this.shouldHighlight(cell) ? 'highlight' : ''}>
+                      <Knight />
+                    </Cell>
+                  </div>
+                )
+              })}
             </div>
           )}
+          <div className="Button">
+            <button disabled={!this.state.initialPosition} onClick={() => this.getNextPositions()}>Próximas</button>
+          </div>
       </div>
     );
   }
--- src/Cell.js
+++ src/Cell.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Cell extends Component {
   render () { 
-    const { id, children, selected } = this.props
+    const { id, children, selected, highlight } = this.props
     const display = selected ? 'visible': 'hidden'
-    return <div className="Cell">
+
+    return <div className={`Cell ${highlight}`}>
             <div className="Cell-id">{ id }</div>
             <div className={display}>{ children }</div>
           </div>
